874542ef443146660d03c49ea6a96634c084cc47
Jin <lochjin@gmail.com>
2023-03-21 13:08:35 +0800

    CGO Conflict with main lib

--- rpc/constants_unix.go
+++ rpc/constants_unix.go
@@ -22,7 +22,7 @@ package rpc
 /*
 #include <sys/un.h>
 
-int max_socket_path_size() {
+int max_socket_path_size2() {
 struct sockaddr_un s;
 return sizeof(s.sun_path);
 }
@@ -30,5 +30,5 @@ return sizeof(s.sun_path);
 import "C"
 
 var (
-	max_path_size = C.max_socket_path_size()
+	max_path_size = C.max_socket_path_size2()
 )
